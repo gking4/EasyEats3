d6986fffdcebbb35525dff3482c9e785239d16ba
daveed19 <38480877+daveed19@users.noreply.github.com>
2018-05-13 15:08:08 -0700

    Add files via upload

---
+++ page2/page2.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { RestaurantPickerPage } from '../restaurant-picker/restaurant-picker';
+
+/**
+ * Generated class for the Page2Page page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+var foodChoices:string[] = ["Hamburger", "Mexican", "Pizza", "Italian",
+                             "Chinese", "Japanese", "Shit"  ]
+var randChoice; 
+
+@IonicPage()
+@Component({
+  selector: 'page-page2',
+  templateUrl: 'page2.html',
+})
+export class Page2Page {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad Page2Page');
+  }
+
+  leverPull(){
+    randChoice = foodChoices[Math.floor(Math.random() * foodChoices.length)];
+    document.getElementById("foodResults").innerHTML = randChoice;
+  };
+  go(){
+    this.navCtrl.push(RestaurantPickerPage);
+  };
+}
